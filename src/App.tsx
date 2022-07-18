e412c2e017faf9ddbfc15a94b7ec296504560ee2
David Zweili <david.zweili@gmail.com>
2022-07-18 17:59:24 +0200

    Log all Pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const getByAxios = async () => {
+  const res = await axios({
+    method: 'get',
+    url: 'https://pokeapi.co/api/v2/pokemon/?limit=1000',
+  });
+  return res.data.results;
+};
+
+const App = () => {
+  getByAxios().then((x) => console.log(x));
+
+  return <div>Hello World</div>;
+};
 
 export default App;
